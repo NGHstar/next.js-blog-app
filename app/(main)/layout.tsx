817e70d233cd65357877ec2c3b7eb04cadd32337
NGHSTAR <nghstarcc@gmail.com>
2026-02-17 11:26:17 +0330

    fix: navbar

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,14 +1,17 @@
-import Navbar from "../../components/web/navbar";
+import { Suspense } from 'react'
+import Navbar from '../../components/web/navbar'
 
 export default function MainLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <>
-      <Navbar />
+      <Suspense fallback={<div className="h-2"></div>}>
+        <Navbar />
+      </Suspense>
       {children}
     </>
-  );
+  )
 }
